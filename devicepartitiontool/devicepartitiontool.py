9d1d387a54e92d4d6d137e5ca725a597c7c6756b
jakeogh <github.com@v6y.net>
2025-10-16 01:08:35 -0700

    auto-commit

--- devicepartitiontool/devicepartitiontool.py
+++ devicepartitiontool/devicepartitiontool.py
@@ -2,27 +2,6 @@
 # -*- coding: utf8 -*-
 # tab-width:4
 
-# pylint: disable=useless-suppression             # [I0021]
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=missing-param-doc               # [W9015]
-# pylint: disable=missing-module-docstring        # [C0114]
-# pylint: disable=fixme                           # [W0511] todo encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-many-public-methods         # [R0904]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
-
 from __future__ import annotations
 
 import logging
@@ -33,14 +12,12 @@ from signal import signal
 
 import click
 import sh
-from asserttool import gvd
 from asserttool import ic
-from asserttool import icp
 from click_auto_help import AHGroup
 from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tv
-from eprint import eprint
+from globalverbose import gvd
 from mounttool import block_special_path_is_mounted
 from pathtool import path_is_block_special
 from warntool import warn
@@ -133,7 +110,12 @@ def write(
     parted_command = sh.Command("parted")
     parted_command = parted_command.bake("-a", "optimal")
     parted_command = parted_command.bake(device.as_posix())
-    parted_command = parted_command.bake("--script", "--", "mkpart", "primary")
+    parted_command = parted_command.bake(
+        "--script",
+        "--",
+        "mkpart",
+        "primary",
+    )
     parted_command = parted_command.bake(filesystem)
     parted_command = parted_command.bake(start, stop)
     result = parted_command()
